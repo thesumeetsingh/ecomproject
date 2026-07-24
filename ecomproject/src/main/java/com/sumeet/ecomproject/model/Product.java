cc5bbf839593a14e93ff6865a68085b49009900d
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-07-24 20:21:42 +0530

    added auto ID generation

--- ecomproject/src/main/java/com/sumeet/ecomproject/model/Product.java
+++ ecomproject/src/main/java/com/sumeet/ecomproject/model/Product.java
@@ -2,6 +2,8 @@ package com.sumeet.ecomproject.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +19,7 @@ import java.util.Date;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private int id;
     private String name;
     private boolean availability;
