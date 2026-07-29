c0b5d41bdd1c0210e9670a7dc4acf2c2cc18fffe
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-07-29 23:37:33 +0530

    login edits

--- ecomproject/src/main/java/com/sumeet/ecomproject/controller/LoginController.java
+++ ecomproject/src/main/java/com/sumeet/ecomproject/controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.sumeet.ecomproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/api")
+@CrossOrigin
 public class LoginController {
+
+    @RequestMapping("/login")
+
 }
