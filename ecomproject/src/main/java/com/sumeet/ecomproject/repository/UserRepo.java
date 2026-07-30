7b16f3010d3c4e51d4f62640fa7703cebb382b6c
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-07-31 00:35:05 +0530

    removed user model, controller, repo

--- ecomproject/src/main/java/com/sumeet/ecomproject/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sumeet.ecomproject.repository;
-
-import com.sumeet.ecomproject.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-
-
-}
