db956ff9dc52c18079ecef7d2cb683c5e1afe609
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-02 23:52:15 +0530

    added user properties

--- backend/src/main/java/com/sumeet/ecomproject/model/User.java
+++ backend/src/main/java/com/sumeet/ecomproject/model/User.java
@@ -1,4 +1,22 @@
 package com.sumeet.ecomproject.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+     @Id
+    private String username;
+     private String firstname;
+     private String lastname;
+     private String email;
+     private Integer phone;
+     private String gender;
+
 }
