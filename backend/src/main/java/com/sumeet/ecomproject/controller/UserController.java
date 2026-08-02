cb135083c33cacb13c662a1c341e47dd30cb51b1
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-08-02 23:59:38 +0530

    created post and get for user

---
+++ backend/src/main/java/com/sumeet/ecomproject/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.sumeet.ecomproject.controller;
+
+
+import com.sumeet.ecomproject.model.User;
+import com.sumeet.ecomproject.service.UserService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController("/api")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{username}")
+    public User getUserById(@PathVariable String username){
+        return userService.getUserById(username);
+    }
+
+    @PostMapping("/{username}/create")
+    public User createUser(@RequestPart User user){
+        return userService.createUser(user);
+    }
+}
---
+++ backend/src/main/java/com/sumeet/ecomproject/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sumeet.ecomproject.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
